b411b511aa8dc9cca5ebe1053ea88a78ca176326
Hendi Santika <hendisantika@gmail.com>
2017-12-21 07:37:35 +0700

    #5 - Add User Repository

---
+++ src/main/java/com/hendisantika/demo/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.hendisantika.demo.repository;
+
+import com.hendisantika.demo.entity.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/***
+ * Created by IntelliJ IDEA
+ * Project Name : demo
+ * Username : Hendi Santika
+ * Date Time : 12/20/2017 4:56 PM
+ * email : hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ */
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+}
